c3e256fd9143891e0c24e14ef3436bb2ea8ea274
s163404 <s163404@gmail.com>
2021-04-30 00:43:33 +0900

    TextFieldのモディファイアのサンプル

--- StackSample/SecondView.swift
+++ StackSample/SecondView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct SecondView: View {
+    @State var value: String = ""
+    @State var isInputError = false
     var body: some View {
-        Text("ナビゲーション")
-            .foregroundColor(.black)
+        VStack {
+            Spacer()
+            VStack {
+                switch isInputError {
+                case true:
+                    TextField("@Stateの動作確認", text: $value)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 3.0)
+                                .stroke(Color.pink, lineWidth: 3.0)
+                                .padding(1)
+                        )
+                        .padding()
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                case false:
+                    TextField("@Stateの動作確認", text: $value)
+                        .padding()
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                }
+                Text(value)
+                    .frame(maxWidth: .infinity, minHeight: 25)
+                    .lineLimit(nil)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .background(Color.init(.lightGray))
+                    .cornerRadius(3.0)
+                    .padding([.horizontal, .bottom], 12)
+            }
+            .background(Color.orange)
+            .cornerRadius(10.0)
+            .padding()
+            Spacer()
+            Button("状態切り替え", action: {
+                self.isInputError.toggle()
+            })
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.init(.link))
+            .cornerRadius(10.0)
+        }
     }
 }
 
