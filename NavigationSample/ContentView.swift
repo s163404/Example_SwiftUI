2be4812378a5f0c7e8e5d6bd8c0d3c2c72553e96
s163404 <s163404@gmail.com>
2021-05-19 00:17:00 +0900

    インデント調整

--- NavigationSample/ContentView.swift
+++ NavigationSample/ContentView.swift
@@ -14,28 +14,28 @@ struct ContentView: View {
         // NavigationViewはコンテナビュー
         // ナビゲーションスタックの階層最上位のViewにNavigationViewをつける
         ZStack(alignment: .top) {
-        NavigationView {
-            VStack {
-                Text("Hello! World")
+            NavigationView {
+                VStack {
+                    Text("Hello! World")
+                        .padding()
+                    // ナビゲーション構造の下にビューを追加し、
+                    // そのビューに遷移するボタンを作成。
+                    NavigationLink(
+                        destination: MySubView(prefix: "A", index: 0, childCount: 2)) {
+                        Text("サブビューへ")
+                    }
                     .padding()
-                // ナビゲーション構造の下にビューを追加し、
-                // そのビューに遷移するボタンを作成。
-                NavigationLink(
-                    destination: MySubView(prefix: "A", index: 0, childCount: 2)) {
-                    Text("サブビューへ")
+                    NavigationLink(
+                        destination: MySubView(prefix: "山梨", index: 0, childCount: 3)) {
+                        Text("山梨")
+                    }
+                    .padding()
+                    NavigationLink(
+                        destination: MySubView(prefix: "湯本", index: 0, childCount: 5)) {
+                        Text("福島県いわき市")
+                    }
                 }
-                .padding()
-                NavigationLink(
-                    destination: MySubView(prefix: "山梨", index: 0, childCount: 3)) {
-                    Text("山梨")
-                }
-                .padding()
-                NavigationLink(
-                    destination: MySubView(prefix: "湯本", index: 0, childCount: 5)) {
-                    Text("福島県いわき市")
-                }
-            }
-            .navigationBarTitle("ホーム", displayMode: .large)
+                .navigationBarTitle("ホーム", displayMode: .large)
             }
             Image(systemName: "gamecontroller")
                 .padding(.top, navigationBarHeight + 8)
